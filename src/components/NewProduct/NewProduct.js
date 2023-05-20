e72481fae0eff284816a30a677f71c8d9fa87d8a
SvetaLantsman <svetlana@curalife.com>
2023-05-20 16:26:07 +0300

    .

--- src/components/NewProduct/NewProduct.js
+++ src/components/NewProduct/NewProduct.js
@@ -1,42 +1,59 @@
 import { useState } from "react";
 import Section from "../UI/Section";
 import ProductForm from "./ProductForm";
+import useHttp from "../../hooks/use-http";
 
 const NewProduct = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendHttpRequest: sendProduct } = useHttp();
+ 
   const [resetFormKey, setResetFormKey] = useState(0);
 
+  const createProduct = (productText, productData) => {
+    const generatedId = productData.name;
+    const createdProduct = { id: generatedId, text: productText };
+    props.onAddProduct(createdProduct);
+    setResetFormKey((prevKey) => prevKey + 1);
+  };
+
   const enterProductHandler = async (productText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://custom-hooks-http-f188a-default-rtdb.firebaseio.com/products.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: productText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+    sendProduct (
+      {
+        endpoint: "https://custom-hooks-http-f188a-default-rtdb.firebaseio.com/products.json",
+        method: "POST",
+        headers: {  "Content-Type": "application/json" },
+        body: JSON.stringify({ text: productText }),  
+       },
+       createProduct.bind(null, productText)
+    );
+    // setIsLoading(true);
+    // setError(null);
+    // try {
+    //   const response = await fetch(
+    //     "https://custom-hooks-http-f188a-default-rtdb.firebaseio.com/products.json",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify({ text: productText }),
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //     }
+    //   );
 
-      if (!response.ok) {
-        throw new Error("Ошибка запроса.");
-      }
+    //   if (!response.ok) {
+    //     throw new Error("Ошибка запроса.");
+    //   }
 
-      const data = await response.json();
+    //   const data = await response.json();
 
-      const generatedId = data.name;
-      const createdProduct = { id: generatedId, text: productText };
+    //   const generatedId = data.name;
+    //   const createdProduct = { id: generatedId, text: productText };
 
-      props.onAddProduct(createdProduct);
-      setResetFormKey((prevKey) => prevKey + 1);
-    } catch (e) {
-      setError(e.message || "Что-то пошло не так...");
-    }
-    setIsLoading(false);
+    //   props.onAddProduct(createdProduct);
+    //   setResetFormKey((prevKey) => prevKey + 1);
+    // } catch (e) {
+    //   setError(e.message || "Что-то пошло не так...");
+    // }
+    // setIsLoading(false);
   };
 
   return (
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,35 +1,36 @@
 import { useCallback, useState } from "react";
 
-const useHttp = (requestOptions, manageData) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    
-    const sendHttpRequest = async () => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const response = await fetch(
-          requestOptions.endpoint, {
-            method: requestOptions.method ? requestOptions.method : "GET",
-            headers: requestOptions.headers ? requestOptions.headers : {},
-            body: requestOptions.body ? JSON.stringify({ text: productText }) : null,
-          }
-        );
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-        if (!response.ok) {
-          throw new Error("Ошибка запроса.");
-        }
+  const sendHttpRequest = useCallback(async (requestOptions, manageData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestOptions.endpoint, {
+        method: requestOptions.method ? requestOptions.method : "GET",
+        headers: requestOptions.headers ? requestOptions.headers : {},
+        body: requestOptions.body ? JSON.stringify(requestOptions.body) : null,
+      });
 
-        const data = await response.json();
-        manageData(data);
-       
-      } catch (err) {
-        setError(err.message || "Что-то пошло не так...");
+      if (!response.ok) {
+        throw new Error("Ошибка запроса.");
       }
-      setIsLoading(false);
-    };
 
-    return {  isLoading, error, sendHttpRequest  };
+      const data = await response.json();
+      manageData(data);
+    } catch (err) {
+      setError(err.message || "Что-то пошло не так...");
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendHttpRequest,
+  };
 };
 
 export default useHttp;
